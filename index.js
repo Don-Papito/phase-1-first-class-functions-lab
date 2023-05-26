bd0394d54dc4b836f601220018b604ab8fa2f60d
Steve <sjd734312@gmail.com>
2023-05-26 15:18:51 -0400

    Last function x__x

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+// Code your solution in this file
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']; 
+
+const returnFirstTwoDrivers = function(){
+    return drivers.slice(0, 2); 
+}
+
+returnFirstTwoDrivers();
+
+const returnLastTwoDrivers = function(){
+    return drivers.slice(2);
+}
+
+returnLastTwoDrivers();
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(number){
+    return function(fare){
+        return fare * number; 
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3); 
+
+function selectDifferentDrivers(drivers, driverSelector) {
+    return driverSelector(drivers);
+ }
+
